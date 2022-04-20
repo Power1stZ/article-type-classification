60469e72456d735cc5ad134e08bf58622285929d
Power1stZ <nattapong1999.dev@gmail.com>
2022-04-21 00:19:07 +0700

    Edit Cache

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ from collections import Counter
 import gensim
 from gensim.models import Word2Vec
 
+
 class MeanEmbeddingVectorizer(object):
     def __init__(self, word2vec):
         self.word2vec = word2vec
@@ -39,6 +40,7 @@ class MeanEmbeddingVectorizer(object):
             ])
 
 #convert to lowercase, strip and remove punctuations
+@st.cache(suppress_st_warning=True)
 def preprocess(text):
     text = text.lower() 
     text=text.strip()  
@@ -53,6 +55,7 @@ def preprocess(text):
 
  
 # STOPWORD REMOVAL
+@st.cache(suppress_st_warning=True)
 def stopword(string):
     a= [i for i in string.split() if i not in stopwords.words('english')]
     return ' '.join(a)
@@ -62,6 +65,7 @@ def stopword(string):
 wl = WordNetLemmatizer()
  
 # This is a helper function to map NTLK position tags
+@st.cache(suppress_st_warning=True)
 def get_wordnet_pos(tag):
     if tag.startswith('J'):
         return wordnet.ADJ
@@ -75,11 +79,13 @@ def get_wordnet_pos(tag):
         return wordnet.NOUN
 
 # Tokenize the sentence
+@st.cache(suppress_st_warning=True)
 def lemmatizer(string):
     word_pos_tags = nltk.pos_tag(word_tokenize(string)) # Get position tags
     a=[wl.lemmatize(tag[0], get_wordnet_pos(tag[1])) for idx, tag in enumerate(word_pos_tags)] # Map the position tag and lemmatize the word/token
     return " ".join(a)
 
+@st.cache(suppress_st_warning=True)
 def finalpreprocess(string):
     return lemmatizer(stopword(preprocess(string)))
 
@@ -108,6 +114,7 @@ category = [
             'Blog,Science'
           ]
 
+@st.cache(suppress_st_warning=True)
 def generate_wordcloud(word):
     wc = WordCloud(
         width=1320,
